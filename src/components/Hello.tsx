2a73bc51111ac3de2232a5e7239f46e73fca9d96
Alican <aerdogan07@gmail.com>
2022-03-13 19:31:27 +0100

    Switched to tailwind

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,18 +1,37 @@
 import * as React from "react";
-import styled from "styled-components";
-import { whereHoverAvailable } from "utils/styles";
+import { styled, extend } from "utils/styles";
 
 export interface HelloProps {
   compiler: string;
   framework: string;
 }
 
-export const HelloStyle = styled.div`
-  ${whereHoverAvailable}
-`;
+export const HelloStyle = styled<{ isActive: boolean }>("h1")(
+  `
+  flex
+  text-blue-500
+  text-4xl
+  `,
+  (v) => [v?.isActive && "text-red-500"]
+);
+
+const NewTitle = extend<{ isActive: boolean; isDisabled: boolean }>(
+  HelloStyle,
+  "h2"
+)(
+  `
+  text-yellow-800
+  `,
+  (v) => [v?.isDisabled && "text-5xl"]
+);
 
 export const Hello: React.FC<HelloProps> = () => {
-  return <HelloStyle>{}</HelloStyle>;
+  return (
+    <>
+      <HelloStyle variant={{ isActive: false }}>{"Hello Style"}</HelloStyle>
+      <NewTitle variant={{ isActive: false }}>{"New Title"}</NewTitle>
+    </>
+  );
 };
 
 Hello.displayName = "Hello";
--- src/utils/styles.tsx
+++ src/utils/styles.tsx
@@ -1,220 +1,103 @@
-import { css } from "styled-components";
+import React, { ReactHTML } from "react";
 
-type CSSArgs = Parameters<typeof css>;
-type CSSReturnType = ReturnType<typeof css>;
+type TClassArg =
+  | string
+  | number
+  | undefined
+  | null
+  | false
+  | Record<string, any>;
 
-const breakpoints = {
-  mobile_sm: 400,
-  mobile: 576,
-  tablet_sm: 768,
-  tablet: 992,
-  desktop: 1440,
-};
+type TCXArgs = (TClassArg | TClassArg[])[];
 
-function mediaFn<T extends Record<string, number>>(breakpoints: T) {
-  const breakpointKeyValuePairs = Object.entries(breakpoints).sort(
-    ([, value1], [, value2]) => value1 - value2
-  );
+function cx(...args: TCXArgs): string {
+  const classes = [];
 
-  const media: Record<string, (...args: CSSArgs) => CSSReturnType> = {};
+  for (const arg of args) {
+    if (!arg) continue;
 
-  for (let i = 0; i < breakpointKeyValuePairs.length; i++) {
-    const [key, value] = breakpointKeyValuePairs[i];
-    media[key] = (...args: CSSArgs) => css`
-      @media (min-width: ${value}px) {
-        ${css(...args)};
+    const argType = typeof arg;
+
+    if (argType === "string" || argType === "number") {
+      classes.push(arg);
+      continue;
+    }
+
+    if (Array.isArray(arg) && arg.length > 0) {
+      const inner = cx(arg);
+      if (inner) {
+        classes.push(inner);
       }
-    `;
+      continue;
+    }
+
+    Object.entries(arg)
+      .filter(([, value]) => !!value)
+      .forEach(([key]) => classes.push(key));
   }
 
-  return media as Record<keyof T, (...args: CSSArgs) => CSSReturnType>;
+  return classes.join(" ").trimEnd().trimStart();
 }
 
-export const media = mediaFn(breakpoints);
+export type TailwindClasses = TCXArgs;
 
-export const whereHoverAvailable = (...args: CSSArgs) => css`
-  @media (hover: none) {
-    ${css(...args)};
-  }
-`;
+type TVariantFn<TVariant extends {}> = (
+  variant: TVariant | undefined
+) => TailwindClasses;
 
-export const colors = {
-  transparent: "transparent",
-  current: "currentColor",
-  black: "#000",
-  white: "#fff",
-  gray: {
-    _100: "#f7fafc",
-    _200: "#edf2f7",
-    _300: "#e2e8f0",
-    _400: "#cbd5e0",
-    _500: "#a0aec0",
-    _600: "#718096",
-    _700: "#4a5568",
-    _800: "#2d3748",
-    _900: "#1a202c",
-  },
-  red: {
-    _100: "#fff5f5",
-    _200: "#fed7d7",
-    _300: "#feb2b2",
-    _400: "#fc8181",
-    _500: "#f56565",
-    _600: "#e53e3e",
-    _700: "#c53030",
-    _800: "#9b2c2c",
-    _900: "#742a2a",
-  },
-  orange: {
-    _100: "#fffaf0",
-    _200: "#feebc8",
-    _300: "#fbd38d",
-    _400: "#f6ad55",
-    _500: "#ed8936",
-    _600: "#dd6b20",
-    _700: "#c05621",
-    _800: "#9c4221",
-    _900: "#7b341e",
-  },
-  yellow: {
-    _100: "#fffff0",
-    _200: "#fefcbf",
-    _300: "#faf089",
-    _400: "#f6e05e",
-    _500: "#ecc94b",
-    _600: "#d69e2e",
-    _700: "#b7791f",
-    _800: "#975a16",
-    _900: "#744210",
-  },
-  green: {
-    _100: "#f0fff4",
-    _200: "#c6f6d5",
-    _300: "#9ae6b4",
-    _400: "#68d391",
-    _500: "#48bb78",
-    _600: "#38a169",
-    _700: "#2f855a",
-    _800: "#276749",
-    _900: "#22543d",
-  },
-  teal: {
-    _100: "#e6fffa",
-    _200: "#b2f5ea",
-    _300: "#81e6d9",
-    _400: "#4fd1c5",
-    _500: "#38b2ac",
-    _600: "#319795",
-    _700: "#2c7a7b",
-    _800: "#285e61",
-    _900: "#234e52",
-  },
-  blue: {
-    _100: "#ebf8ff",
-    _200: "#bee3f8",
-    _300: "#90cdf4",
-    _400: "#63b3ed",
-    _500: "#4299e1",
-    _600: "#3182ce",
-    _700: "#2b6cb0",
-    _800: "#2c5282",
-    _900: "#2a4365",
-  },
-  indigo: {
-    _100: "#ebf4ff",
-    _200: "#c3dafe",
-    _300: "#a3bffa",
-    _400: "#7f9cf5",
-    _500: "#667eea",
-    _600: "#5a67d8",
-    _700: "#4c51bf",
-    _800: "#434190",
-    _900: "#3c366b",
-  },
-  purple: {
-    _100: "#faf5ff",
-    _200: "#e9d8fd",
-    _300: "#d6bcfa",
-    _400: "#b794f4",
-    _500: "#9f7aea",
-    _600: "#805ad5",
-    _700: "#6b46c1",
-    _800: "#553c9a",
-    _900: "#44337a",
-  },
-  pink: {
-    _100: "#fff5f7",
-    _200: "#fed7e2",
-    _300: "#fbb6ce",
-    _400: "#f687b3",
-    _500: "#ed64a6",
-    _600: "#d53f8c",
-    _700: "#b83280",
-    _800: "#97266d",
-    _900: "#702459",
-  },
-};
+export function styled<TVariant extends {}>(type: keyof ReactHTML | "input") {
+  type P = keyof ReactHTML | "input";
+  type PropsType = { variant?: Partial<TVariant> } & (P extends "input"
+    ? React.InputHTMLAttributes<HTMLInputElement>
+    : React.HTMLAttributes<P>) &
+    React.ClassAttributes<P>;
 
-export const boxShadow = {
-  xs: "box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.05);",
-  sm: "box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);",
-  default:
-    "box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);",
-  md: "box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);",
-  lg: "box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);",
-  xl: "box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04);",
-  xl_2: "box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);",
-  inner: "box-shadow: inset 0 2px 4px 0 rgba(0, 0, 0, 0.06);",
-  outline: "box-shadow: 0 0 0 3px rgba(66, 153, 225, 0.5);",
-  none: "box-shadow: none;",
-};
+  return (classes: string, fn?: TVariantFn<Partial<TVariant>>) => {
+    const ReturnedComponent = React.forwardRef(
+      ({ variant, ...props }: PropsType, ref: any) => {
+        const dynamicClasses = fn ? fn(variant) : [];
+        const className = cx(classes, ...dynamicClasses);
+        return React.createElement(type, { ...props, className, ref });
+      }
+    );
 
-export const spacing = {
-  px: "1px",
-  _0: "0",
-  _1: "0.25rem",
-  _2: "0.5rem",
-  _3: "0.75rem",
-  _4: "1rem",
-  _5: "1.25rem",
-  _6: "1.5rem",
-  _8: "2rem",
-  _10: "2.5rem",
-  _12: "3rem",
-  _16: "4rem",
-  _20: "5rem",
-  _24: "6rem",
-  _32: "8rem",
-  _40: "10rem",
-  _48: "12rem",
-  _56: "14rem",
-  _64: "16rem",
-};
+    (ReturnedComponent as any).__styles = { classes, fn };
 
-export const fontSize = {
-  xs: "font-size: 0.75rem;",
-  sm: "font-size: 0.875rem;",
-  base: "font-size: 1rem;",
-  lg: "font-size: 1.125rem;",
-  xl: "font-size: 1.25rem;",
-  xl_2: "font-size: 1.5rem;",
-  xl_3: "font-size: 1.875rem;",
-  xl_4: "font-size: 2.25rem;",
-  xl_5: "font-size: 3rem;",
-  xl_6: "font-size: 4rem;",
-};
+    return ReturnedComponent as any as React.FC<PropsType> & {
+      __styles: { classes: string; fn?: TVariantFn<TVariant> };
+    };
+  };
+}
 
-export function compose(
-  ...args: (ReturnType<typeof css> | false | undefined)[]
+export function extend<TVariant extends {}>(
+  Component: React.FC<any> & {
+    __styles: {
+      classes: string;
+      fn?: TVariantFn<TVariant> | undefined;
+    };
+  },
+  type: keyof ReactHTML | "input"
 ) {
-  return css`
-    ${args
-      .map((c) => {
-        if (!c) {
-          return "";
-        }
-        return c.join("");
-      })
-      .join("\n")}
-  `;
+  const {
+    __styles: { classes: baseClasses, fn: baseFn },
+  } = Component;
+  function Styled<TNewVariant extends TVariant>(
+    classes: string,
+    fn?: TVariantFn<TNewVariant>
+  ) {
+    const newClasses = `${baseClasses} ${classes}`;
+    let newFn: TVariantFn<TNewVariant> | undefined = fn;
+    if (baseFn) {
+      if (fn) {
+        newFn = (variant: TNewVariant | undefined) => {
+          return [...baseFn(variant), ...fn(variant)];
+        };
+      } else {
+        newFn = baseFn;
+      }
+    }
+    return styled<TNewVariant>(type)(newClasses, newFn as any);
+  }
+
+  return Styled;
 }
